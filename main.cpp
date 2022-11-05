d4fc35d259dfb95d27da748f71ccf774c6a0ef52
Rowdy2018 <117493382+Rowdy2018@users.noreply.github.com>
2022-11-05 13:09:30 +1000

    Add C++ file
    This is the first c++ file

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+int cnt = 0;
+
+int main(){
+  cout << "Enter the name of file << endl;
+  cin >> cnt;
+  cout << "The entered value is " << cnt << endl;
+}
